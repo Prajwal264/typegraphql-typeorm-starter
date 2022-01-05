656621ad68907e4bf5cfafcf2262a2d91ebf1610
Prajwal264 <prajwalp264@gmail.com>
2022-01-05 21:34:18 +0530

    (feat): added support for graphql errors

--- source/src/types/user.type.ts
+++ source/src/types/user.type.ts
@@ -1,12 +1,5 @@
-import { Field, ObjectType } from 'type-graphql';
+import { ObjectType } from 'type-graphql';
 import User from '../entities/user.entity';
-import Error from './error.type';
 
 @ObjectType()
-export class UserResponse {
-  @Field({ nullable: true })
-    data?: User;
-
-  @Field(() => [Error], { nullable: true })
-    errors?: Error[];
-}
+export class UserResponse extends User {}
